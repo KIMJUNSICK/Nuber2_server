f24e9e23b474fe2f7f7f75bee1ef1a597eac37ed
Kim Jun Sik <jinaganda5@gmail.com>
2019-09-25 09:52:52 +0900

    Options & handleListen Ftn for Graphql server

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { Options } from "graphql-yoga";
+import app from "./app";
+
+// module first, library second
+
+const PORT: number | string = process.env.PORT || 4000;
+const PLAYGROUND_ENDPOINT: string = "/playground";
+const GRAPHQL_ENDPOINT: string = "/graphql";
+
+const appOptions: Options = {
+  port: PORT,
+  playground: PLAYGROUND_ENDPOINT,
+  endpoint: GRAPHQL_ENDPOINT
+};
+
+const handleListen = () =>
+  console.log(`Listening on: http://localhost:${PORT}`);
+
+app.start(appOptions, handleListen);
